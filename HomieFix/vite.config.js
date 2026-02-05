82f16688e9badc825ab03d09da6f108bb4ecb251
sstutiiii <stutisharma0110@gmail.com>
2026-02-05 14:28:49 +0530

    Update vite.config.js

--- HomieFix/vite.config.js
+++ HomieFix/vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/HomieFix/',   // ⭐ VERY IMPORTANT
   plugins: [react()],
   build: {
-    outDir: 'docs',  // output folder changed from default 'dist' to 'docs'
+    outDir: 'docs',
   },
 })
+
